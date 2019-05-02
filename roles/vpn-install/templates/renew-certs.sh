8875372b58da1c11fc90ca5b5e3f4afc10ae665c
Mattia Natali <matitalatina@gmail.com>
2019-05-02 21:34:20 +0200

    Bug fix during renewing cert

--- roles/vpn-install/templates/renew-certs.sh
+++ roles/vpn-install/templates/renew-certs.sh
@@ -2,4 +2,4 @@
 cp /etc/letsencrypt/live/{{ domain }}/chain.pem /etc/swanctl/x509ca/
 cp /etc/letsencrypt/live/{{ domain }}/privkey.pem /etc/swanctl/private/
 cp /etc/letsencrypt/live/{{ domain }}/fullchain.pem /etc/swanctl/x509/
-/usr/sbin/swanctl --load-creds
+/usr/sbin/swanctl --load-all
